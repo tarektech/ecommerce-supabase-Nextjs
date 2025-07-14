83f9fdafa9aa14e51d6d44ad91b895bfb23f2530
tarek alzein <tarekalzein97@gmail.com>
2025-07-14 10:49:09 +0300

    Update configuration and package name
    - Change build output directory settings in next.config.mjs.
    - Update package name in package.json to reflect project identity.

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  distDir: './dist', // Changes the build output directory to `./dist/`.
-  basePath: isProd ? '/ecommerce-supabase-next' : '',
   images: {
     remotePatterns: [
       {
-        hostname: '**',
+        hostname: "**",
       },
       {
-        hostname: 'fakestoreapi.com',
+        hostname: "fakestoreapi.com",
       },
     ],
     dangerouslyAllowSVG: true,
-    unoptimized: process.env.NODE_ENV === 'development', // Disable optimization in development
+    unoptimized: process.env.NODE_ENV === "development", // Disable optimization in development
   },
 };
 
